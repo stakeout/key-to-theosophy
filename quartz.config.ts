32d849021fee4b0ae8564df005f2220c9aa2493e
Vadim Novash <novash@tut.by>
2025-05-23 15:55:33 +0300

    Quartz sync: May 23, 2025, 3:55 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Дорогой Цвете",
+    pageTitle: "Дорогой Цвете с ❤️",
     pageTitleSuffix: "345",
     enableSPA: true,
     enablePopovers: true,
